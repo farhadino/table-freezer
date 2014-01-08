562540d0adfc1e58dcb4aee4d5538338dee8435a
farhadino <farhadino@gmail.com>
2014-01-08 22:09:23 +0100

    initial commit

---
+++ tablefreezer.jquery.js
@@ -0,0 +1,13 @@
+// .. as a jquery plugin
+
+define([ "jquery" ], function($) {
+
+	function createTableRowFloater(selector_str) {
+
+	}
+
+	function initializeTable(selector_str, topOffset) {
+
+	}
+
+}
